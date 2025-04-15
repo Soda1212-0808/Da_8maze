687c39df10b2a9d3693a47b9a4adeb4cda098e78
Soda1212-0808 <soda940808@gmail.com>
2025-04-15 09:31:40 +0100

    s

--- DS_plexon/peri_event_raster__v7.m
+++ DS_plexon/peri_event_raster__v7.m
@@ -1,12 +1,6 @@
 clear all
-% 设置事件窗口
 
-psth_window = [-2,2];
-
-% 设置直方图参数
-bin_size = 0.05; % 直方图的bin大小（单位：秒）
-smooth_window=20;
-Path='E:\SDdata\WT\each mice\'
+Path='E:\SDdata\WT\each mice\';
 % Path = 'H:\CA3_reprocess\each mice\';    % 设置数据存放的文件夹路径
 animals={'DCA3-9','DCA3-10','DCA3-11','DCA3-12','DCA3-14','DCA3-17','DCA3-20'};
 newfolderName = 'PSTH';
@@ -14,6 +8,17 @@ if exist(fullfile(Path,newfolderName), 'dir') ~= 7
     mkdir(fullfile(Path,newfolderName));
     disp(['Folder "', newfolderName, '" created.']);
 end
+
+% set parameters
+smooth_window=100;
+raster_window = [-2,2];
+psth_bin_size = 0.005;
+t_bins = raster_window(1):psth_bin_size:raster_window(2);
+
+
+all_response_idx=cell(length(animals),1);
+all_psth=cell(length(animals),1);
+all_raster=cell(length(animals),1);
 for curr_animal=1:length(animals)
     % 获取文件夹中的所有内容
     animal=animals{curr_animal};
@@ -21,62 +26,142 @@ for curr_animal=1:length(animals)
     % 获取所有子文件夹的名称
     recording_files = {contents(([contents.isdir] & ~ismember({contents.name}, {'.', '..'}))).name};
 
-    for curr_file=1:2
+    for curr_day=1:length(recording_files)
         %处理event数据
-        event_file=dir(fullfile(Path, animal ,recording_files{curr_file} ,'*data_m1.csv'));
+        event_file=dir(fullfile(Path, animal ,recording_files{curr_day} ,'*data_m1.csv'));
         data_event=csvread(fullfile(event_file.folder , event_file.name));
-        %         [event_idx,event_type]=findgroups(data_event(:,1));
-        % 指定排序顺序
-        desiredOrder = [26, 62, 17, 71, 35, 53];
-        % 创建排序索引
-        [~, sortIndex] = ismember(data_event(:, 1), desiredOrder);
-        % 根据desiredOrder排序
-        [~, sortOrder] = sort(sortIndex);
-        % 对矩阵进行排序
-        data_event_sorted = data_event(sortOrder, :);
-        data_event_sorted_correct= data_event_sorted(data_event_sorted(:,2)==1,:);
 
-        event_times{1}=sort(data_event_sorted(:,3));
-        event_times{2}=sort(data_event_sorted(:,4));
-        event_times{3}=sort(data_event_sorted(:,5));
-        event_times{4}=sort(data_event_sorted(:,6));
-        event_times{5}=sort(data_event_sorted(:,7));
-        event_times{6}=sort(data_event_sorted(:,8));
-        event_times{7}=sort(data_event_sorted(:,9));
-        event_times{8}=sort(data_event_sorted(:,10));
+        events_name= unique (data_event(:,1));
+        event_times = arrayfun(@(event) arrayfun(@(id) sort(data_event(data_event(:,1) == id, event)),...
+            events_name, 'UniformOutput', false),...
+            3:10, 'UniformOutput', false);
 
-
-
-        event_times{1}=sort(data_event_sorted(data_event_sorted(:,1)==17,4));
-        event_times{2}=sort(data_event_sorted(data_event_sorted(:,1)==71,4));
-        event_times{3}=sort(data_event_sorted(data_event_sorted(:,1)==26,4));
-        event_times{4}=sort(data_event_sorted(data_event_sorted(:,1)==62,4));
-        event_times{5}=sort(data_event_sorted(data_event_sorted(:,1)==35,4));
-        event_times{6}=sort(data_event_sorted(data_event_sorted(:,1)==53,4));
-  event_times{7}=sort(data_event_sorted(:,4));
-
-
-        neuron_files=dir(fullfile(Path ,animal , recording_files{curr_file} ,'*.t'));
+        neuron_files=dir(fullfile(Path ,animal , recording_files{curr_day} ,'*.t'));
         spikes_all = arrayfun(@(f) readmclusttfile(fullfile(f.folder, f.name))'/10000, ...
             neuron_files, 'UniformOutput', false);
 
+        templates_all = cell2mat(arrayfun(@(idx, n) repmat(idx, n, 1),...
+            (1:numel(cellfun(@numel, spikes_all)))', cellfun(@numel, spikes_all), 'UniformOutput', false));
+        [spike_times_timelite, sort_idx] = sort(cell2mat(spikes_all));
+        spike_templates = templates_all(sort_idx);
 
 
-templates_all = cell2mat(arrayfun(@(idx, n) repmat(idx, n, 1),...
-    (1:numel(cellfun(@numel, spikes_all)))', cellfun(@numel, spikes_all), 'UniformOutput', false));
-[spike_times_timelite, sort_idx] = sort(cell2mat(spikes_all));
-spike_templates = templates_all(sort_idx);
+       
+        [psth_smooth,raster,~]=...
+            cellfun(@(y) cellfun(@(x) ap.psth(spike_times_timelite,x,spike_templates,'smoothing',smooth_window,...
+            'window',raster_window,'bin_size',psth_bin_size),y,'UniformOutput',false)...
+            ,event_times,'UniformOutput',false);
 
 
 
-  all_unit_psth_smooth_norm=...
-   cellfun(@(x) normalize( ap.psth(spike_times_timelite,x,spike_templates,'smoothing',100,...
-   'window',[-2 2],'bin_size',0.001),2)...
-   ,event_times,'UniformOutput',false);
-     
+        psth_smooth_norm=cellfun(@(y) cellfun(@(x) normalize(x,2),y,'UniformOutput',false),...
+            psth_smooth,'UniformOutput',false);
+
+
+        response_unit= cellfun(@(y) cellfun(@(x) x>2,y,'UniformOutput',false),...
+            psth_smooth_norm,'UniformOutput',false)
+
+        % responsive neurons
+        min_len = 20;
+
+        % 卷积检测每行中是否存在长度为4的连续1
+        is_match = cellfun(@(y)  cellfun(@(x) conv2(x(:,find(t_bins>-0.5&t_bins<1)), ones(1, min_len), 'valid') == min_len,...
+            y,'UniformOutput',false),...
+            response_unit,'UniformOutput',false);  % 结果为逻辑矩阵
+
+        % 对每一行找第一个为 true 的位置（用 max 找 true 的列索引）
+        [pos_val, response_idx] = cellfun(@(y)  cellfun(@(x) max(x, [], 2),y,'UniformOutput',false),...
+            is_match,'UniformOutput',false);  % pos_val 是是否找到，pos_idx 是起始列
+    
+        figure
+        tiledlayout(6, 8);
+
+        for curr_event=1:6
+            for curr_timepoint=1:8
+
+                nexttile
+                temp_data=psth_smooth_norm{curr_timepoint}{curr_event}(response_idx{curr_timepoint}{curr_event}>1,:);
+
+
+                [a,b]=sort(response_idx{curr_timepoint}{curr_event}(response_idx{curr_timepoint}{curr_event}>1));
+
+                imagesc(t_bins ,[],temp_data(b,:))
+
+                clim([2 2.2])
+                colormap(ap.colormap('WK'))
+
+
+
+            end
+        end
+        sgtitle([animal ' day ' num2str(curr_day)])
+        drawnow
+        all_response_idx{curr_animal}{curr_day}= ...
+            cell2mat(cellfun(@(x) reshape(x, [1, 1, size(x)]), cat(2,response_idx{:}), 'UniformOutput', false));
+        all_psth{curr_animal}{curr_day}= ...
+            cell2mat(cellfun(@(x) reshape(x, [1, 1, size(x)]), cat(2,psth_smooth_norm{:}), 'UniformOutput', false));
+        all_raster{curr_animal}{curr_day}=raster;
+    end
+end
+
+    
+
+
+
+all_psth_curr_day1=cellfun(@(x)  x{1}  ,all_psth,'UniformOutput',false );
+all_psth_curr_day1_1=cat(3,all_psth_curr_day1{:});
+
+all_response_idx_curr_day1=cellfun(@(x)  x{1}  ,all_response_idx,'UniformOutput',false );
+all_response_idx_curr_day1_1=cat(3,all_response_idx_curr_day1{:});
+
+
+all_response_idx_curr_day1_align = nan(size(all_response_idx_curr_day1_1));
+all_response_idx_curr_day1_align(all_response_idx_curr_day1_1 ~= 1) = t_bins(all_response_idx_curr_day1_1(all_response_idx_curr_day1_1 ~= 1));
+
+%%
+
+figure
+tiledlayout(6, 8);
+
+for curr_event=1:6
+for curr_timepoint=1:8
   
- 
+      nexttile
 
+temp_idx=  permute(all_response_idx_curr_day1_1(curr_event,curr_timepoint,:),[3,2,1]);
+if ismember(curr_timepoint,[2 3 6 7])
+    selected_idx=temp_idx>100;
+
+else
+selected_idx=temp_idx>1;
+end
+
+
+[a,b]=sort(temp_idx(selected_idx));
+temp_data=permute(all_psth_curr_day1_1(curr_event,curr_timepoint,...
+    selected_idx,:),[3,4,2,1]);
+
+
+
+imagesc(t_bins ,[],temp_data(b,:))
+
+      clim([0 2.2])
+      colormap(ap.colormap('WK'))
+
+
+
+
+
+
+    end
+    end
+
+
+
+
+
+
+  %% correlation  相关性分析
 spike_binning_t = 0.05; % seconds
 spike_binning_t_edges = nanmin(spike_times_timelite):spike_binning_t:nanmax(spike_times_timelite);
 
@@ -113,13 +198,5 @@ raster_window = [-2,2];
 psth_bin_size = 0.001;
 t_bins = raster_window(1):psth_bin_size:raster_window(2);
 t_centers = conv2(t_bins,[1,1]/2,'valid');
-figure
-hold on
-  for curr_i=1:7
-      nexttile
-      imagesc(t_bins,[],all_unit_psth_smooth_norm{curr_i}(leafOrder,:))
-      clim([0.8 1])
-      colormap(ap.colormap('WK'))
 
-  end
   drawnow
